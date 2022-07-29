7c725bc18724cd6cef5725f4f4ec735e171625a4
emin <mu.eminb_97@hotmail.com>
2022-07-29 17:37:44 +0300

    post methods added, docs are remaining

--- src/sidekick-api.js
+++ src/sidekick-api.js
@@ -1,117 +1,125 @@
-const axios = require('axios');
-const common = require('../common.js');
+const axios = require("axios");
+const common = require("../common.js");
 
+class SidekickApi {
+  /**
+   * @param {string} apiKey - Your sidekick project's apikey
+   * @param {string} authToken - Your auth token from sidekick
+   * @param {string} sidekickHost
+   */
+  constructor({ apiKey, authToken }, sidekickHost = common.SIDEKICK_HOST) {
+    this.sidekickHost = sidekickHost;
+    this.apiKey = apiKey;
+    this.authToken = authToken;
+  }
 
-class SidekickApi{
+  /**
+   * @param {object} tracepointLocation - Tracepoint information to be removed
+   * @param {string} tracepointLocation.fileName - Full name of file in version control
+   * @param {number} tracepointLocation.line - Line of tracepoint
+   * @param {string} tracepointLocation.email - Your sidekick email
+   */
+  removeTracepoint(tracepointLocation) {
+    return this.#removePoint(common.TRACEPOINT_ENDPOINT, tracepointLocation);
+  }
 
-    
+  /**
+   * @param {object} logpointLocation - Logpoint information to be removed
+   * @param {string} logpointLocation.fileName - Full name of file in version control
+   * @param {number} logpointLocation.line - Line of tracepoint
+   * @param {string} logpointLocation.email - Your sidekick email
+   */
+  removeLogpoint(logpointLocation) {
+    return this.#removePoint(common.LOGPOINT_ENDPOINT, logpointLocation);
+  }
 
-    /**
-     * @param {string} apiKey - Your sidekick project's apikey
-     * @param {string} authToken - Your auth token from sidekick
-     * @param {string} sidekickHost 
-     */
-    constructor({apiKey, authToken},sidekickHost=common.SIDEKICK_HOST){
-        this.sidekickHost=sidekickHost;
-        this.apiKey=apiKey;
-        this.authToken=authToken
-    }
+  /**
+   *
+   * @param {object} params
+   * @param {object} params.applicationFilters -wrong doc for now !!!!
+   * @param {string} params.applicationFilters.name
+   * @param {string} params.applicationFilters.version
+   * @param {string} params.applicationFilters.stage
+   * @param {Array=} params.applicationFilters.customTags
+   *
+   * @param {string} params.fileName
+   * @param {number} params.lineNo
+   * @param {number=} params.expireSecs
+   * @param {number=} params.expireCount
+   * @param {boolean=} params.enableTracing
+   * @param {boolean=} params.persist
+   */
+  putTracepoints(params) {
+    return this.#createPoint(common.TRACEPOINT_ENDPOINT, params);
+  }
 
+  /**
+   *
+   * @param {object} params
+   * @param {object} params.applicationFilters - wrong doc for now !!!!
+   * @param {string} params.applicationFilters.name
+   * @param {string} params.applicationFilters.version
+   * @param {string} params.applicationFilters.stage
+   * @param {Array=} params.applicationFilters.customTags
+   *
+   * @param {string} params.fileName
+   * @param {number} params.lineNo
+   * @param {string|""} params.logExpression
+   * @param {number=} params.expireSecs
+   * @param {number=} params.expireCount
+   * @param {boolean=} params.enableTracing
+   * @param {boolean=} params.persist
+   */
+  putLogpoint(params) {
+    return this.#createPoint(common.LOGPOINT_ENDPOINT, params);
+  }
 
+  #createHeaders({isPost}) {
+    let headers = {
+      ApiKey: this.apiKey,
+      Authorization: "Token " + this.authToken,
+      "Content-Type": isPost ? "application/json" : "text/plain",
+    };
+    return headers;
+  }
 
-    /**
-     * @param {object} tracepointLocation - Tracepoint information to be removed
-     * @param {string} tracepointLocation.fileName - Full name of file in version control
-     * @param {number} tracepointLocation.line - Line of tracepoint
-     * @param {string} tracepointLocation.email - Your sidekick email
-     */
-    removeTracepoint (tracepointLocation) {
-          return this.#removePoint(common.TRACEPOINT_ENDPOINT,tracepointLocation)
-    }
+  async #createPoint(endpoint, params) {
+    let config = {
+      headers: this.#createHeaders({isPost:true}),
+    };
+    await axios
+      .post(this.sidekickHost + endpoint, params, config)
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    /**
-    * @param {object} logpointLocation - Logpoint information to be removed
-    * @param {string} logpointLocation.fileName - Full name of file in version control
-    * @param {number} logpointLocation.line - Line of tracepoint
-    * @param {string} logpointLocation.email - Your sidekick email
-    */
-    removeLogpoint = function (logpointLocation) {
-        return this.#removePoint(common.LOGPOINT_ENDPOINT,logpointLocation)
-    }
-
-
-
-    #createHeaders() {
-        return {  
-            "ApiKey" : this.apiKey,
-            "Authorization": "Token "+this.authToken,
-            'Content-Type' : 'text/plain'
-        };
-      }
-
-      #removePoint(endpoint,tracepointLocation){
-        let config = {
-            headers: this.#createHeaders(),
-            data: tracepointLocation.fileName+"::"+tracepointLocation.line+"::"+tracepointLocation.email
-          }
-          axios.delete(this.sidekickHost + endpoint,  config)
-        .then(res => {
-            console.log(res);
-            return res;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-      }
+  async #removePoint(endpoint, tracepointLocation) {
+    let config = {
+      headers: this.#createHeaders({isPost:false}),
+      data:
+        tracepointLocation.fileName +
+        "::" +
+        tracepointLocation.line +
+        "::" +
+        tracepointLocation.email,
+    };
+    await axios
+      .delete(this.sidekickHost + endpoint, config)
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
 
-
-
-var putTracepoints= function ({auth,params}) {
-  
-}
-
-
-var putLogpoint = function (params) {
- 
-}
-
-
-
-var getTracepoints = function (auth,params) {
- 
-}
-
-var getLogpoints = function (params) {
- 
-}
 module.exports = {
-    SidekickApi
-  };
-
-function sidekickPost(endpoint,params){
-    axios
-    .post(SIDEKICK_HOST+endpoint, params)
-    .then(res => {
-        console.log(res);
-        return res;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
-function sidekickPost(endpoint,params){
-    axios
-    .post(SIDEKICK_HOST+endpoint, params)
-    .then(res => {
-        console.log(res);
-        return res;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
-
-
-
-
+  SidekickApi,
+};
