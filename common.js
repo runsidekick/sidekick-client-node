0039041f228676f5faecefca2ae83fec95f2ba4e
emin <mu.eminb_97@hotmail.com>
2022-08-02 12:18:50 +0300

    Default api migrated to production environment

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-const  SIDEKICK_HOST="https://api.service.runsidekick.me"
+const  SIDEKICK_HOST="https://api.service.runsidekick.com"
 const  LOGPOINT_ENDPOINT="/api/v1/logpoint"
 const  TRACEPOINT_ENDPOINT="/api/v1/tracepoint"
 
