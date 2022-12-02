e9b7137ea96a4e2a8c159bb424c2df07f5fa06a4
Yakup Yasin Kalafat <yasin@thundra.io>
2022-12-02 14:28:37 +0300

    Change Websocket Library (#14)

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { onTrigger } = require('./src/client.js')
 const { SidekickApi } = require('./src/sidekick-api.js')
 const { SidekickTestApi } = require('./src/sidekick-test-api.js')
 module.exports = {
-  onTrigger,SidekickApi, SidekickTestApi
+  onTrigger, SidekickApi, SidekickTestApi
 }
